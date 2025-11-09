d6a011cae5c3b84b77f7007b6b14e70330b007de
dexoxed.skllfi <dexoxed.skllfi@gmail.com>
2025-11-09 20:45:07 +0000

    Fix: Update google-services plugin version to 4.4.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,5 +18,5 @@ rootProject.name = "AuroraBridgeDeviceOptimizer"
 include(":app")
 
 plugins {
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
