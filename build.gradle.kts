cd11251025c0c1b1ebca38fee8d9917e69e3a51d
dexoxed.skllfi <dexoxed.skllfi@gmail.com>
2025-11-09 19:46:28 +0000

    Fix: Apply classpath fix for Firebase Performance plugin conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,21 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // This is the fix, adapted from Firebase docs for a known issue.
+        // We forcefully add the performance plugin to the classpath with an exclusion
+        // to resolve a dependency conflict (Guava).
+        classpath("com.google.firebase:perf-plugin:1.4.2") {
+            exclude(group = "com.google.guava", module = "guava-jdk5")
+        }
+    }
+}
+
 plugins {
     kotlin("android") version "1.9.21" apply false
     id("com.android.application") version "8.1.0" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21" apply false
-    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
 }
