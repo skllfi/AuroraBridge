4e83d4d82a6352613aff51a43175598ae01ee00e
dexoxed.skllfi <dexoxed.skllfi@gmail.com>
2025-11-09 20:42:39 +0000

    Fix: Declare google-services plugin in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,7 @@ dependencyResolutionManagement {
 
 rootProject.name = "AuroraBridgeDeviceOptimizer"
 include(":app")
+
+plugins {
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
